e9ddb08e8976669c6da635f51256525f403760d2
stevenhao <stevenhao@gmail.com>
2016-11-15 13:55:13 -0500

    clean dead code

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ m.route.mode = "hash";
 var main = document.querySelector('#main');
 m.route(main, "/read", {
     "/read": Read, // place the Read component in #main
-    "/write": Write, // this is an unused route, for now
-    "/slider": Slider, // this is an unused route, for now
 });
